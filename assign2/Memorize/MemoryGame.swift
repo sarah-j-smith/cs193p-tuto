d3a3d4dcfe1143a89ba9d456978ee87c5248e8b1
Sarah Smith <sarah@smithsoft.com.au>
2022-09-04 15:26:06 +1000

    Cards are now shuffled in-game

--- assign2/Memorize/MemoryGame.swift
+++ assign2/Memorize/MemoryGame.swift
@@ -20,6 +20,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
             cards.append(Card(id: pairIndex * 2, content: content))
             cards.append(Card(id: pairIndex * 2 + 1, content: content))
         }
+        cards = cards.shuffled()
     }
     
     mutating func choose(_ card: Card) {
